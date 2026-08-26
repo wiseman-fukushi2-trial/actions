3df803ddf280e00a1123270b922dcf26dc6a9603
RKuji <RKuji@mx1.wiseman.co.jp>
2026-08-26 12:13:05 +0900

    test

--- actions/build/BuildProjects.ps1
+++ actions/build/BuildProjects.ps1
@@ -12,7 +12,7 @@ foreach ($vbproj in $args) {
 	$msBuildArgs = ($vbproj.Contains("front")) ?
 		@(
 			"$vbproj"
-			"/p:TargetFrameworkSDKToolsDirectory=""C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools"""
+			"/p:TargetFrameworkSDKToolsDirectory=C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools"
 			"/clp:ErrorsOnly"
 			"/nologo"
 		) :
